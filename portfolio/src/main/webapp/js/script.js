532dfa54c601e8ae408f480dc4f674b6b548c693
mybffjustin <juh369@g.harvard.edu>
2022-06-07 14:42:31 -0500

    Add js and photo

--- portfolio/src/main/webapp/js/script.js
+++ portfolio/src/main/webapp/js/script.js
@@ -1,3 +1,32 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+/**
+ * Adds a random about to the page.
+ */
+function addRandomAbout() {
+	const abouts =
+		['Student 👨‍🎓', 'Software Engineer 👨‍💻', 'Web Software Developer 👨‍💻'];
+
+	// Pick a random about.
+	const about = abouts[Math.floor(Math.random() * abouts.length)];
+
+	// Add it to the page.
+	const aboutContainer = document.getElementById('about-container');
+	aboutContainer.innerText = about;
+}
+
 /******************************************************************************************************************************
 Waypoints
 *******************************************************************************************************************************/ 
