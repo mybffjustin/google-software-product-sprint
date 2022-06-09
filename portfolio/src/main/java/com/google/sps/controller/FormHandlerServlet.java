7fff2e9aa9d86d156a8366a7bfdfa75085ef8415
mybffjustin <juh369@g.harvard.edu>
2022-06-08 23:02:40 -0500

    Add FormHandlerServlet.java

---
+++ portfolio/src/main/java/com/google/sps/controller/FormHandlerServlet.java
@@ -0,0 +1,43 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.sps.controller;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/form-handler")
+public class FormHandlerServlet extends HttpServlet {
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        String name = request.getParameter("name-input");
+        String email = request.getParameter("email-input");
+        String comment = request.getParameter("comment-input");
+
+        // // Print the input so you can see it in the server logs.
+        System.out.println("name: " + name);
+        System.out.println("email: " + email);
+        System.out.println("comment: " + comment);
+
+        // Write the input to the response so the user can see it.
+        response.setContentType("text/html;");
+        response.getWriter().println("<p>Name: " + name + "</p>");
+        response.getWriter().println("<p>Email: " + email + "</p>");
+        response.getWriter().println("<p>Comment: " + comment + "</p>");
+    }
+}
--- portfolio/src/main/webapp/js/script.js
+++ portfolio/src/main/webapp/js/script.js
@@ -201,39 +201,5 @@ $(window).load(function () {
             $.waypoints('refresh');
         }
     });
-
-    $('#servicesSlider').flexslider({
-        animation: "slide",
-        directionNav: false,
-        controlNav: true,
-        touch: true,
-        pauseOnHover: true,
-        start: function () {
-            $.waypoints('refresh');
-        }
-    });
-
-    $('#teamSlider').flexslider({
-        animation: "slide",
-        directionNav: false,
-        controlNav: true,
-        touch: true,
-        pauseOnHover: true,
-        start: function () {
-            $.waypoints('refresh');
-        }
-    });
-
-    $('#clientSlider').flexslider({
-        animation: "slide",
-        directionNav: false,
-        controlNav: true,
-        touch: true,
-        pauseOnHover: true,
-        start: function () {
-            $.waypoints('refresh');
-        }
-    });
-
 });
 
